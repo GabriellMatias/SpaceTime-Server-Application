4b7140d267ce6a95ecc12fff3376ce23876c9e33
GabriellMatias <gabriel.goiasat@gmail.com>
2023-05-15 21:17:43 -0300

    :rocket: Feat: Setting basics configs

---
+++ src/serve.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/hellou', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running... 🚀')
+})
